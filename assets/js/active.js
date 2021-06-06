dbba63c8a5ccf5a1c4a303da1551bec840bfbb42
guiaforhelp <contato@guiaforhelp.com.br>
2021-06-05 23:26:36 -0300

    teste

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,4 +1,5 @@
-document.querySelector('.eicon-plus').addEventListener('click', function(){
-    //console.log('TESTE TESTE TESTE');
-    window.alert('teste');
-});
+window.onload = function(){
+    document.querySelector('.eicon-plus').addEventListener('click', function(){
+        console.log('TESTE TESTE TESTE');
+    });
+};
