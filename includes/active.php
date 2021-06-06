8a7995821150f4e18c0b76b2c0cac95a45e270e3
guiaforhelp <contato@guiaforhelp.com.br>
2021-06-05 23:54:37 -0300

    teste

--- includes/active.php
+++ includes/active.php
@@ -1,5 +1,11 @@
 <?php
-wp_enqueue_script('elements', ELEMENTOR_ASSETS_URL. 'js/active.js', array(), '1.0', false);
+wp_enqueue_script(
+    'elements', 
+    ELEMENTOR_ASSETS_URL. 'js/active.js', 
+    [], 
+    '1.0', 
+    true
+);
 
 add_action('wp_enqueue_scripts', 'add_element_scripts');
 ?>
