6c9d4eb6a55423facf08fc83f1f8dd2e294805af
guiaforhelp <contato@guiaforhelp.com.br>
2021-06-07 22:31:29 -0300

    t

--- includes/editor-templates/global.php
+++ includes/editor-templates/global.php
@@ -47,37 +47,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 <script type="text/template" id="tmpl-elementor-tag-controls-stack-empty">
 	<?php echo __( 'This tag has no settings.', 'elementor' ); ?>
 </script>
-<div class="elementor-inner">
-<div class="elementor-section-wrap">
-<div class="elementor-add-section-inner miqueias">
-		<div class="elementor-add-section-close">
-			<i class="eicon-close" aria-hidden="true"></i>
-			<span class="elementor-screen-only"><?php echo __( 'Close', 'elementor' ); ?></span>
-		</div>
-		<div class="elementor-add-new-section">
-			<div class="elementor-add-section-area-button elementor-add-section-button" title="<?php echo __( 'Add New Section', 'elementor' ); ?>">
-				<i class="eicon-plus"></i>
-			</div>
-			<div class="elementor-add-section-area-button elementor-add-template-button" title="<?php echo __( 'Add Template', 'elementor' ); ?>">
-				<i class="eicon-folder"></i>
-			</div>
-			<div class="elementor-add-section-drag-title"><?php echo __( 'Drag widget here', 'elementor' ); ?></div>
-		</div>
-		<div class="elementor-select-preset">
-			<div class="elementor-select-preset-title"><?php echo __( 'Select your Structure', 'elementor' ); ?></div>
-			<ul class="elementor-select-preset-list">
-				<#
-					var structures = [ 10, 20, 30, 40, 21, 22, 31, 32, 33, 50, 60, 34 ];
-
-					_.each( structures, function( structure ) {
-					var preset = elementor.presetsFactory.getPresetByStructure( structure ); #>
-
-					<li class="elementor-preset elementor-column elementor-col-16" data-structure="{{ structure }}">
-						{{{ elementor.presetsFactory.getPresetSVG( preset.preset ).outerHTML }}}
-					</li>
-					<# } ); #>
-			</ul>
-		</div>
-	</div>
-	</div>
-	</div>
